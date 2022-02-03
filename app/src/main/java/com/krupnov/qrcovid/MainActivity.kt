f3c082979fa57bbfbbc72cc3bd0f7fc318b2336a
Крупнов Михаил <krupnovmn@gmail.com>
2022-02-03 19:32:26 +0300

    Add Qr generator

--- app/src/main/java/com/krupnov/qrcovid/MainActivity.kt
+++ app/src/main/java/com/krupnov/qrcovid/MainActivity.kt
@@ -2,10 +2,35 @@ package com.krupnov.qrcovid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidmads.library.qrgenearator.QRGContents
+import androidmads.library.qrgenearator.QRGEncoder
+import com.google.zxing.WriterException
 
 class MainActivity : AppCompatActivity() {
+
+    var im: ImageView? = null
+    var bGenerate: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        im = findViewById(R.id.imageView)
+        bGenerate = findViewById(R.id.generateButton)
+        bGenerate?.setOnClickListener {
+            generateQrCode("Поставь лайк")
+        }
+    }
+
+    private fun generateQrCode(text: String) {
+        val qrGenerator = QRGEncoder(text, null, QRGContents.Type.TEXT, 500)
+        try {
+            val bMap = qrGenerator.bitmap
+            im?.setImageBitmap(bMap)
+        } catch (e: WriterException){
+
+        }
     }
 }
